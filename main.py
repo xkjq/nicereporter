725908c0e3cdc5f6bf50ef98c598f271204dbcd6
Ross <xkjq@ymail.com>
2025-05-05 15:48:01 +0100

    .

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from report_stroke import report_stroke  # Import the report_stroke page
 def main():
     ui.label('Report Generatort')
     ui.button('Stroke', on_click=lambda: ui.navigate.to('/report_stroke'))
-    ui.button('Stroke', on_click=lambda: ui.navigate.to('/report_stroke'))
-    ui.button('Stroke', on_click=lambda: ui.navigate.to('/report_stroke'))
 
     @ui.page('/about')
     def about_page():
